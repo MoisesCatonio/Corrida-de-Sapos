201329a6e835b92c048c3dc36b99fdd8aa70e5d0
Moises Oliveira Catonio de Araújo <moisesc@A307-27L.imd.local>
2018-03-27 18:26:12 -0300

    Melhorando os comments

--- INCLUDE/sapo.hpp
+++ INCLUDE/sapo.hpp
@@ -21,27 +21,44 @@ class Sapo{
 		//Declara um sapo com seus atributos mutáveis.
 		Sapo(int id, int d, int qtd, int fp);
 
+		//Retorna o atributo "identificador" do sapo.
 		int getId();
+
+		//Altera o atributo "identificador" do sapo.
 		void setId(int id);
 
+		//Retorna o atributo de distância percorrida do sapo.
 		int getDist_perc();
+		
+		//Altera o atributo de distância percorrida do sapo.
 		void setDist_perc(int dist);
-
+		
+		//Retorna o atributo de quantidade de pulos dada do sapo.
 		int getQtd_pulos();
+
+		//Altera o atributo de quantidade de pulos dada do sapo.
 		void setQtd_pulos(int qtd);
 
+		//Retorna o atributo equivalente a força do pulo do sapo.
 		float getFuerza();
+
+		//Altera o atributo equivalente a força do pulo do sapo.
 		void setFuerza(float fp); // Fp é uma piadinha com Frog Power/ Horse Power.
 
+		//Cria um vetor com valores de pulo que variam conforme seu indice e a adição de uma variável.
 		void createCommonJump();
 
+		//Função que gera a quantidade de pulos do sapo baseado no vetor de pulos criado na função createCommonJump.
 		void showYourCommon();
-		
+
+		//Mostra os valores presentes no vetor com os pulos possíveis do sapo.
 		int pular();
 
 		int Win();
 
 		static int chegada;
+		
+		//Retorna a quantidade de metros que será utilizada como chegada.
 		static int getChegada();
 };
 
--- SRC/sapo.cpp
+++ SRC/sapo.cpp
@@ -10,47 +10,39 @@
 		fuerza = fp;
 
 	}
-	//retorna o atributo "identificador" do sapo.
+	
 	int Sapo::getId(){
 		return identificador;
 	}
 	
-	//Altera o atributo "identificador" do sapo.
 	void Sapo::setId(int id){
 		identificador = id;
 	}
 
-	//retorna o atributo de distância percorrida do sapo.
 	int Sapo::getDist_perc(){
 		return dist_perc;
 	}
 
-	//Altera o atributo de distância percorrida do sapo.
 	void Sapo::setDist_perc(int dist){
 		dist_perc = dist_perc + dist;
 	}
 
-	//retorna o atributo de quantidade de pulos dada do sapo.
 	int Sapo::getQtd_pulos(){
 		return qtd_pulos;
 	}
 
-	//Altera o atributo de quantidade de pulos dada do sapo.
 	void Sapo::setQtd_pulos(int qtd){
 		qtd_pulos = qtd_pulos + qtd;
 	}
 
-	//retorna o atributo equivalente a força do pulo do sapo.
 	float Sapo::getFuerza(){
 		return fuerza;
 	}
 
-	//Altera o atributo equivalente a força do pulo do sapo.
 	void Sapo::setFuerza(float fp){
 		fuerza = fp;
 	}
-
-	//Cria um vetor com valores de pulo que variam conforme seu indice e a adição de uma variável. 
+ 
 	void Sapo::createCommonJump(){
 		for (int i = 0; i < 6; i++)
 		{
@@ -58,21 +50,18 @@
 		}
 	}
 
-	//Função que gera a quantidade de pulos do sapo baseado no vetor de pulos criado na função createCommonJump.
 	int Sapo::pular(){
 		int key = rand() % 5;
 		int jump = fuerza * capacidade[key];
 		return jump;
 	}
 
-	//Mostra os valores presentes no vetor com os pulos possíveis do sapo.
 	void Sapo::showYourCommon(){
 		for (int i = 0; i < 6; i++){
 			cout<<capacidade[i]<<endl;
 		} 
 	}
-
-	//Retorna a quantidade de metros que será utilizada como chegada. 
+ 
 	int Sapo::getChegada(){
 		return chegada;
 	}
